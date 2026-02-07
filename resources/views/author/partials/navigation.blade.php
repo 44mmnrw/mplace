cdc36d0504ea716c28abc1502f516927dba3a690
Yuri.R <44mmnrw@gmail.com>
2026-02-07 15:52:35 +0300

    Deploy 2026-02-07 15:52:34

--- resources/views/author/partials/navigation.blade.php
+++ resources/views/author/partials/navigation.blade.php
@@ -22,7 +22,7 @@
                 <circle cx="8" cy="8" r="6" stroke="currentColor" stroke-width="1.5"/>
                 <path d="M8 5V8L10 10" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
             </svg>
-            <span>Витрина</span>
+            <span>Марина!</span>
         </a>
 
         <a href="#" class="author-nav__item">
@@ -30,14 +30,14 @@
                 <path d="M2 3H14C14.55 3 15 3.45 15 4V12C15 12.55 14.55 13 14 13H2C1.45 13 1 12.55 1 12V4C1 3.45 1.45 3 2 3Z" stroke="currentColor" stroke-width="1.5"/>
                 <path d="M1 5H15" stroke="currentColor" stroke-width="1.5"/>
             </svg>
-            <span>Вышивка</span>
+            <span>Научись</span>
         </a>
 
         <a href="#" class="author-nav__item">
             <svg class="author-nav__icon" viewBox="0 0 16 16" fill="none">
                 <path d="M8 2L10.5 7H15L11.5 10L13 15L8 12L3 15L4.5 10L1 7H5.5L8 2Z" stroke="currentColor" stroke-width="1.5" stroke-linejoin="round"/>
             </svg>
-            <span>Скрапбукинг</span>
+            <span>Мыслить</span>
         </a>
 
         <a href="#" class="author-nav__item">
@@ -45,7 +45,7 @@
                 <circle cx="8" cy="8" r="6.5" stroke="currentColor" stroke-width="1.5"/>
                 <path d="M8 5V8M8 11V12" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
             </svg>
-            <span>Декор</span>
+            <span>Абстрактно</span>
         </a>
 
         <a href="#" class="author-nav__item">
@@ -53,7 +53,7 @@
                 <path d="M3 2H13C14.1 2 15 2.9 15 4V12C15 13.1 14.1 14 13 14H3C1.9 14 1 13.1 1 12V4C1 2.9 1.9 2 3 2Z" stroke="currentColor" stroke-width="1.5"/>
                 <path d="M5 7L8 10L13 5" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
-            <span>Украшения</span>
+            <span>!</span>
         </a>
 
         <a href="#" class="author-nav__item">
@@ -61,7 +61,7 @@
                 <path d="M2.5 3H13.5C14.3284 3 15 3.67157 15 4.5V11.5C15 12.3284 14.3284 13 13.5 13H2.5C1.67157 13 1 12.3284 1 11.5V4.5C1 3.67157 1.67157 3 2.5 3Z" stroke="currentColor" stroke-width="1.5"/>
                 <path d="M3 5.5H13" stroke="currentColor" stroke-width="1.5"/>
             </svg>
-            <span>Популярное</span>
+            <span>!</span>
         </a>
     </div>
 </nav>
